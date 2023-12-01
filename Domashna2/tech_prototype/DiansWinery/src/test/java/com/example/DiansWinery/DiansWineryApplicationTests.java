526fd289a10605c3e8041570c67b3de8dcc5994f
Stefan Veljanoski <stefan_veljanoski@yahoo.com>
2023-12-01 21:24:54 +0100

    Update add backend

---
+++ Domashna2/tech_prototype/DiansWinery/src/test/java/com/example/DiansWinery/DiansWineryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DiansWinery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiansWineryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
