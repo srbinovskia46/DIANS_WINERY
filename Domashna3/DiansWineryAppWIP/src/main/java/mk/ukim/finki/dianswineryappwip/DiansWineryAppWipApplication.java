5f2be7996a9c0c142848ad2f6caf1e44a0d317e9
srbinovskia46 <andrej.srbinovski@students.finki.ukim.mk>
2023-12-05 11:06:29 +0100

    Initialize Domashna 3

---
+++ Domashna3/DiansWineryAppWIP/src/main/java/mk/ukim/finki/dianswineryappwip/DiansWineryAppWipApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.dianswineryappwip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiansWineryAppWipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiansWineryAppWipApplication.class, args);
+    }
+
+}
---
+++ Domashna3/DiansWineryAppWIP/src/test/java/mk/ukim/finki/dianswineryappwip/DiansWineryAppWipApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.dianswineryappwip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiansWineryAppWipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
